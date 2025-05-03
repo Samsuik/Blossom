1c178c1e0aaa6b373cb5d418c30f8a9f785c99a3
Samsuik <kfian294ma4@gmail.com>
2025-05-03 18:09:52 +0100

    Fix paperclip script

--- scripts/paperclip.sh
+++ scripts/paperclip.sh
@@ -6,8 +6,8 @@ cp ./Blossom-Server/target/server*-SNAPSHOT.jar ./work/Paperclip/blossom-1.8.8.j
 cp ./work/1.8.8/1.8.8.jar ./work/Paperclip/minecraft_server.1.8.8.jar
 cd ./work/Paperclip
 mvn clean package -Dmcver=1.8.8 "-Dpaperjar=$basedir/work/Paperclip/blossom-1.8.8.jar" "-Dvanillajar=$basedir/work/Paperclip/minecraft_server.1.8.8.jar"
-cd $basedir
-mkdir $basedir/build
+cd "$basedir"
+mkdir "$basedir/build"
 cp ./work/Paperclip/assembly/target/paperclip*.jar ./build/blossom-paperclip.jar
 
 echo ""
